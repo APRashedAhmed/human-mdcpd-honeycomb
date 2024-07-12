9366c15c7bb4d83e7dd38156298077cf0ee34a47
Abdullah <aprashedahmed@gmail.com>
2024-07-12 10:36:02 -0400

    CCV OH fix 1

--- src/App/deployments/firebase.js
+++ src/App/deployments/firebase.js
@@ -7,7 +7,7 @@ import "firebase/compat/firestore";
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
-  databaseURL: process.env.REACT_APP_databaseURL,
+  // databaseURL: process.env.REACT_APP_databaseURL,
   projectId: process.env.REACT_APP_projectId ?? "no-firebase",
   storageBucket: process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
